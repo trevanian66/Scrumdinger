d46737a075b86c432f1071728a6d9b9d23fa6009
trevanian66 <kwablah@hotmail.com>
2024-04-10 15:02:52 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Kobby Awadzi on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
